3b4d4f621a9e8e5df47709181b5be8ce63cc9b1e
yzh <yzh@yzhdeMacBook-Pro.local>
2020-08-13 20:59:12 +0800

    revise the case

--- spring-boot-hello/src/main/java/com/neo/controller/HelloController.java
+++ spring-boot-hello/src/main/java/com/neo/controller/HelloController.java
@@ -1,13 +1,22 @@
 package com.neo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${user.name}")
+        private  String name;
+    @Value("${user.age}")
+        private String age;
+
+
 	
     @RequestMapping("/")
-    public String index() {
-        return "Hello Spring Boot 2.0!";
+    public String showUserInfo() {
+        return "user.name:" + name + "<br/>"
+                +"user.age:" + age + "<br/>";
     }
 }
---
+++ spring-boot-hello/src/test/java/com/neo/TestCase.java
@@ -0,0 +1,39 @@
+package com.neo;
+
+
+public class TestCase {
+       public static void main(String args[]){
+           InterfaceExample ie2 = new InterfaceImplementExample();
+           ie2.func1();
+           System.out.println(InterfaceExample.x);
+
+
+
+       }
+
+    public interface InterfaceExample {
+        void func1();
+
+        default void func2(){
+            System.out.println("func2");
+        }
+
+        int x = 123;
+        // int y;               // Variable 'y' might not have been initialized
+        public int z = 0;       // Modifier 'public' is redundant for interface fields
+        // private int k = 0;   // Modifier 'private' not allowed here
+        // protected int l = 0; // Modifier 'protected' not allowed here
+        // private void fun3(); // Modifier 'private' not allowed here
+    }
+
+
+    public static class InterfaceImplementExample implements InterfaceExample {
+        @Override
+        public void func1() {
+            System.out.println("func1");
+        }
+    }
+
+
+
+}
